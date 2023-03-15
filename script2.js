d209dfdaad2316be0ad42d93c09e94798cd00fac
Artem <artem.dzhavadyan1@gmail.com>
2023-03-15 17:16:54 +0200

    first commit

---
+++ script2.js
@@ -0,0 +1,26 @@
+/*jshint esversion: 8 */
+
+// alert("Hello");
+
+// const result =  confirm("Are you here?");
+
+// console.log(result);
+
+// const answer = +prompt("Vam est 18?", "Eneter value");
+
+// console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt('your name: ', '');
+answers[1] = prompt('your surname: ', '');
+answers[2] = prompt('your age: ', '');
+
+console.log(typeof(answers));
+console.log(typeof(null));
+
+
+
+
+
+
---
+++ script3.js
@@ -0,0 +1,9 @@
+/*jshint esversion: 8 */
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Kostya';
+
+alert(`Hello, ${user}`);
